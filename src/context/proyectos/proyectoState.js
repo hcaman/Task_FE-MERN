d2a278b0260fc4dff47e65d1cc1a2f804824ace9
hugo cm <hugocmdesign@gmail.com>
2021-08-29 01:37:08 +0200

    Add the dispatch with the action inside reducer

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -1,14 +1,23 @@
 import React, { useReducer } from 'react';
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
+import { FORMULARIO_PROYECTO } from '../../types';
 
 const ProyectoState = props => {
   const initialState = { formulario: false };
 
   const [state, dispatch] = useReducer(proyectoReducer, initialState);
 
+  const showForm = () => {
+    dispatch({
+      type: FORMULARIO_PROYECTO
+    });
+  };
+
   return (
-    <proyectoContext.Provider value={{ formulario: state.formulario }}>
+    <proyectoContext.Provider
+      value={{ formulario: state.formulario, showForm }}
+    >
       {props.children}
     </proyectoContext.Provider>
   );
