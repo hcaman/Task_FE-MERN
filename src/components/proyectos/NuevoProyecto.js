49d109d9af58f5faf21dcf36ffd769fd735266e4
hugo cm <hugocmdesign@gmail.com>
2021-08-29 01:24:36 +0200

    Add function for show hide the form

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const NuevoProyecto = () => {
+  const proyectosContext = useContext(proyectoContext);
+  const { formulario } = proyectosContext;
+
   const [proyecto, setProyecto] = useState({
     nombre: ''
   });
@@ -23,21 +27,23 @@ const NuevoProyecto = () => {
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
-      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Nombre Proyecto"
-          name="nombre"
-          value={nombre}
-          onChange={onChangeProyecto}
-        />
-        <input
-          type="submit"
-          className="btn btn-block btn-primario"
-          placeholder="Agregar Proyecto"
-        />
-      </form>
+      {formulario && (
+        <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Nombre Proyecto"
+            name="nombre"
+            value={nombre}
+            onChange={onChangeProyecto}
+          />
+          <input
+            type="submit"
+            className="btn btn-block btn-primario"
+            placeholder="Agregar Proyecto"
+          />
+        </form>
+      )}
     </>
   );
 };
