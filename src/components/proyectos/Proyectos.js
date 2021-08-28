a32c0130140cac3ab4becf37f8566d46c3018a8f
hugo cm <hugocmdesign@gmail.com>
2021-08-29 00:28:28 +0200

    Add all view of projects

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Sidebar from '../layout/Sidebar';
+import Barra from '../layout/Barra';
 
 const Proyectos = () => {
-  return <div>Proyectos</div>;
+  return (
+    <div className="contenedor-app">
+      <Sidebar />
+      <div className="seccion-principal">
+        <Barra />
+        <main>
+          <div className="contenedor-tareas"></div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Proyectos;
