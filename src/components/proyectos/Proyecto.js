c7dec83ede5ba440463250164efea27e40520780
hugo cm <hugocmdesign@gmail.com>
2021-08-29 02:51:07 +0200

    Add fn for select the project

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const Proyecto = ({ proyecto }) => {
+  const proyectosContext = useContext(proyectoContext);
+  const { currentProject } = proyectosContext;
+
+  const selectProject = () => currentProject(proyecto.id);
+
   return (
     <li>
-      <button type="button" className="btn btn-blank">
+      <button type="button" className="btn btn-blank" onClick={selectProject}>
         {proyecto.nombre}
       </button>
     </li>
