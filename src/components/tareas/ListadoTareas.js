5c993b78c6fdf9185025ba1f56eb850e9444e506
hugo cm <hugocmdesign@gmail.com>
2021-08-29 02:51:32 +0200

    Show the list only when one project is selected

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 import Tarea from './Tarea';
 
 const ListadoTareas = () => {
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  if (!proyecto) return <h2>Selecciona un projecto</h2>;
+
+  const [proyectoActual] = proyecto;
+
   const tareasProyecto = [
     { id: 1, nombre: 'Elegir plataforma', estado: true },
     { id: 2, nombre: 'Elegir colores', estado: false },
     { id: 3, nombre: 'Elegir plataforma de pago', estado: false },
     { id: 4, nombre: 'Elegir hosting', estado: true }
   ];
+
   return (
     <>
-      <h2>Proyecto: Tienda Virtual</h2>
+      <h2>Proyecto: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
         {!tareasProyecto?.length ? (
           <li className="tarea">No hay tareas</li>
