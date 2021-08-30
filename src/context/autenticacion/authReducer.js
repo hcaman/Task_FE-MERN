34194dee706f775cd2de5109df2d0672da65b7e3
hugo cm <hugocmdesign@gmail.com>
2021-08-30 19:54:09 +0200

    Create context for handle the auth

---
+++ src/context/autenticacion/authReducer.js
@@ -0,0 +1,15 @@
+import {
+  REGISTRO_EXITOSO,
+  REGISTRO_ERROR,
+  LOGIN_EXITOSO,
+  LOGIN_ERROR,
+  OBTENER_USUARIO,
+  CERRAR_SESION,
+} from '../../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
