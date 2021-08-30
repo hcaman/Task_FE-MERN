a56cd49ca432f1df3f196eb3714171966b2e3dd0
hugo cm <hugocmdesign@gmail.com>
2021-08-30 08:47:07 +0200

    Add context, reducer and state for Tareas

---
+++ src/context/tareas/tareaReducer.js
@@ -0,0 +1,15 @@
+import {
+  FORMULARIO_PROYECTO,
+  OBTENER_PROYECTO,
+  AGREGAR_PROYECTO,
+  ELIMINAR_PROYECTO,
+  PROYECTO_ACTUAL,
+  VALIDAR_FORM,
+} from '../../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ src/context/tareas/tareaState.js
@@ -0,0 +1,49 @@
+import React, { useReducer } from 'react';
+import { v4 as uuid } from 'uuid';
+import tareaContext from './tareaContext';
+import TareaReducer from './tareaReducer';
+import { TAREAS_PROYECTO } from '../../types';
+
+const TareaState = (props) => {
+  const tareasProyecto = [
+    { id: 1, proyectoId: 1, nombre: 'Elegir plataforma', estado: true },
+    { id: 2, proyectoId: 2, nombre: 'Elegir colores', estado: false },
+    {
+      id: 3,
+      proyectoId: 3,
+      nombre: 'Elegir plataforma de pago',
+      estado: false,
+    },
+    { id: 4, proyectoId: 4, nombre: 'Elegir hosting', estado: true },
+    { id: 1, proyectoId: 4, nombre: 'Elegir plataforma', estado: true },
+    { id: 2, proyectoId: 3, nombre: 'Elegir colores', estado: false },
+    {
+      id: 3,
+      proyectoId: 2,
+      nombre: 'Elegir plataforma de pago',
+      estado: false,
+    },
+    { id: 4, proyectoId: 1, nombre: 'Elegir hosting', estado: true },
+  ];
+
+  const initialState = {
+    tareas: tareasProyecto,
+  };
+
+  const [state, dispatch] = useReducer(TareaReducer, initialState);
+
+  const getTareas = (proyectoId) => {
+    dispatch({
+      type: TAREAS_PROYECTO,
+      payload: proyectoId,
+    });
+  };
+
+  return (
+    <tareaContext.Provider value={{ tareas: state.tareas, getTareas }}>
+      {props.children}
+    </tareaContext.Provider>
+  );
+};
+
+export default TareaState;
