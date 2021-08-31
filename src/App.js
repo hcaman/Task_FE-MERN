ab6f9656c68568a85ae4bc54bba52bf9f367c59d
hugo cm <hugocmdesign@gmail.com>
2021-08-31 21:30:03 +0200

    Add token inside every Route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import ProyectoState from './context/proyectos/proyectoState';
 import TareaState from './context/tareas/tareaState';
 import AlertaState from './context/alertas/alertaState';
 import AuthState from './context/autenticacion/authState';
+import tokenAuth from './config/token';
+
+const token = localStorage.getItem('token');
+if (token) {
+  tokenAuth(token);
+}
 
 function App() {
   return (
