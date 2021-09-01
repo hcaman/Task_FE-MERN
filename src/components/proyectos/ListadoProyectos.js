80bd60243a3670f02c2ba0bfceee44caf145e04b
hugo cm <hugocmdesign@gmail.com>
2021-09-01 18:13:31 +0200

    Add handler error into ListadoProyectos

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -2,22 +2,32 @@ import React, { useContext, useEffect } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Proyecto from './Proyecto';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import AlertaContext from '../../context/alertas/alertaContext';
 
 const ListadoProyectos = () => {
   const proyectosContext = useContext(proyectoContext);
-  const { proyectos, getProjects } = proyectosContext;
+  const { mensaje, proyectos, getProjects } = proyectosContext;
+
+  const alertContext = useContext(AlertaContext);
+  const { alerta, showAlert } = alertContext;
 
   useEffect(() => {
+    if (mensaje) {
+      showAlert(mensaje.msg, mensaje.categoria);
+    }
     getProjects();
-  }, []);
+  }, [mensaje]);
 
   if (!proyectos?.length) return <p>No hay projectos, crea uno!</p>;
 
   return (
     <ul className="listado-proyectos">
+      {alerta && (
+        <div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>
+      )}
       <TransitionGroup>
         {proyectos.map((proyecto) => (
-          <CSSTransition key={proyecto.id} timeout={200} className="proyecto">
+          <CSSTransition key={proyecto._id} timeout={200} className="proyecto">
             <Proyecto proyecto={proyecto} />
           </CSSTransition>
         ))}
